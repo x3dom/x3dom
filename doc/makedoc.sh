9d4525e99a59638e836809b5c33d786af61a13d0
peschler <peschler@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2009-10-08 18:51:05 +0000

    Added basic documentation files (based on jsdoc_toolkit).
    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@16 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

---
+++ doc/makedoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+JSDOCDIR="/home/peter/downloads/jsdoc_toolkit-2.3.2/jsdoc-toolkit"
+CURDIR=`pwd`
+pushd .
+cd $JSDOCDIR
+java -jar jsrun.jar app/run.js -a -t=templates/jsdoc -r 2 $CURDIR/../src -d=$CURDIR
+popd
