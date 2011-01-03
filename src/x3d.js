004ee8418c37d62a873f4ea1ff481263206b5146
yjung <yjung@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2011-01-03 18:04:30 +0000

    impl. skyColor field in Bgnd. and ColorInterpolator nodetest_skyColor.xhtml
    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@839 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

--- src/x3d.js
+++ src/x3d.js
@@ -5643,6 +5643,7 @@ x3dom.registerNodeType(
             x3dom.nodeTypes.X3DInterpolatorNode.superClass.call(this, ctx);
             
 			this.addField_MFFloat(ctx, 'key', []);
+            this.addField_SFFloat(ctx, 'set_fraction', 0);
         },
         {
             linearInterp: function (t, interp) {
@@ -5670,10 +5671,10 @@ x3dom.registerNodeType(
         function (ctx) {
             x3dom.nodeTypes.OrientationInterpolator.superClass.call(this, ctx);
             
-            if (ctx.xmlNode.hasAttribute('keyValue'))
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue'))
                 this._vf.keyValue = x3dom.fields.MFRotation.parse(ctx.xmlNode.getAttribute('keyValue'));
             else
-                this._vf.keyValue = [];
+                this._vf.keyValue = new x3dom.fields.MFRotation();
             
             this._fieldWatchers.fraction = this._fieldWatchers.set_fraction = [ function (msg) {
                 var value = this.linearInterp(msg, function (a, b, t) { return a.slerp(b, t); });
@@ -5691,13 +5692,59 @@ x3dom.registerNodeType(
         function (ctx) {
             x3dom.nodeTypes.PositionInterpolator.superClass.call(this, ctx);
             
-            if (ctx.xmlNode.hasAttribute('keyValue'))
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue'))
                 this._vf.keyValue = x3dom.fields.MFVec3f.parse(ctx.xmlNode.getAttribute('keyValue'));
             else
-                this._vf.keyValue = [];
+                this._vf.keyValue = new x3dom.fields.MFVec3f();
             
             this._fieldWatchers.fraction = this._fieldWatchers.set_fraction = [ function (msg) {
-                var value = this.linearInterp(msg, function (a, b, t) { return a.multiply(1.0-t).add(b.multiply(t)); });
+                var value = this.linearInterp(msg, function (a, b, t) {
+                            return a.multiply(1.0-t).add(b.multiply(t)); });
+                this.postMessage('value_changed', value);
+            } ];
+        }
+    )
+);
+
+// ### NormalInterpolator ###
+x3dom.registerNodeType(
+    "NormalInterpolator",
+    "Interpolation",
+    defineClass(x3dom.nodeTypes.X3DInterpolatorNode,
+        function (ctx) {
+            x3dom.nodeTypes.NormalInterpolator.superClass.call(this, ctx);
+            
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue'))
+                this._vf.keyValue = x3dom.fields.MFVec3f.parse(ctx.xmlNode.getAttribute('keyValue'));
+            else
+                this._vf.keyValue = new x3dom.fields.MFVec3f();
+            
+            this._fieldWatchers.fraction = this._fieldWatchers.set_fraction = [ function (msg) {
+                var value = this.linearInterp(msg, function (a, b, t) {
+                            return a.multiply(1.0-t).add(b.multiply(t)).normalize(); });
+                this.postMessage('value_changed', value);
+            } ];
+        }
+    )
+);
+
+// ### ColorInterpolator ###
+x3dom.registerNodeType(
+    "ColorInterpolator",
+    "Interpolation",
+    defineClass(x3dom.nodeTypes.X3DInterpolatorNode,
+        function (ctx) {
+            x3dom.nodeTypes.ColorInterpolator.superClass.call(this, ctx);
+            
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue'))
+                this._vf.keyValue = x3dom.fields.MFColor.parse(ctx.xmlNode.getAttribute('keyValue'));
+            else
+                this._vf.keyValue = new x3dom.fields.MFColor();
+            
+            this._fieldWatchers.fraction = this._fieldWatchers.set_fraction = [ function (msg) {
+                var value = this.linearInterp(msg, function (a, b, t) {
+                            // FIXME; perform color interpolation in HSV space
+                            return a.multiply(1.0-t).add(b.multiply(t)); });
                 this.postMessage('value_changed', value);
             } ];
         }
@@ -5712,10 +5759,11 @@ x3dom.registerNodeType(
         function (ctx) {
             x3dom.nodeTypes.ScalarInterpolator.superClass.call(this, ctx);
             
-            if (ctx.xmlNode.hasAttribute('keyValue'))
-                this._vf.keyValue = Array.map(ctx.xmlNode.getAttribute('keyValue').split(/\s+/), function (n) { return +n; });
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue'))
+                this._vf.keyValue = Array.map(ctx.xmlNode.getAttribute('keyValue').split(/\s+/), 
+                                                                    function (n) { return +n; });
             else
-                this._vf.keyValue = [];
+                this._vf.keyValue = new x3dom.fields.MFFloat();
 			
             this._fieldWatchers.fraction = this._fieldWatchers.set_fraction = [ function (msg) {
                 var value = this.linearInterp(msg, function (a, b, t) { return (1.0-t)*a + t*b; });
@@ -5734,7 +5782,7 @@ x3dom.registerNodeType(
             x3dom.nodeTypes.CoordinateInterpolator.superClass.call(this, ctx);
             
             this._vf.keyValue = [];
-            if (ctx.xmlNode.hasAttribute('keyValue')) {
+            if (ctx && ctx.xmlNode.hasAttribute('keyValue')) {
                 var arr = x3dom.fields.MFVec3f.parse(ctx.xmlNode.getAttribute('keyValue'));
                 var key = this._vf.key.length > 0 ? this._vf.key.length : 1;
                 var len = arr.length / key;
