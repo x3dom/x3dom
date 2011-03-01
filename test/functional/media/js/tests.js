3d0fb3f0ab04082b741532837b9f018a18d297e7
thyrell <thyrell@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2011-03-01 14:36:54 +0000

    Started work on putting acceptance tests in seperate directory
    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@951 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

---
+++ test/functional/media/js/tests.js
@@ -0,0 +1,29 @@
+// Tests loader
+// adds menu as well as stylesheet to tests
+// the only file to include in the HTML for tests is this one
+
+var init = function() {
+	var head = document.getElementsByTagName('head')[0];
+	var css = document.createElement('link');
+	css.type = 'text/css';
+	css.rel = 'stylesheet';
+	css.href = 'media/css/tests.css';
+	css.media = 'screen';
+	head.appendChild(css);
+
+	var nav = document.createElement("nav");
+	nav.innerHTML = '<a href="index.html">back to test index</a>';
+	document.body.insertBefore(nav, document.body.firstChild);
+
+//	<nav><a href="index.html">back to test index</a></nav>
+};
+
+
+// dom loaded shorthand http://www.kryogenix.org/days/2007/09/26/shortloaded
+(function(i) {var u =navigator.userAgent;var e=/*@cc_on!@*/false; var st = 
+setTimeout;if(/webkit/i.test(u)){st(function(){var dr=document.readyState;
+if(dr=="loaded"||dr=="complete"){i()}else{st(arguments.callee,10);}},10);}
+else if((/mozilla/i.test(u)&&!/(compati)/.test(u)) || (/opera/i.test(u))){
+document.addEventListener("DOMContentLoaded",i,false); } else if(e){     (
+function(){var t=document.createElement('doc:rdy');try{t.doScroll('left');
+i();t=null;}catch(e){st(arguments.callee,0);}})();}else{window.onload=i;}})(init);
