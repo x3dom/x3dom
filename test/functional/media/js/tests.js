7d46091a3efafdebb808b0661c9924b4abd636ff
thyrell <thyrell@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2011-03-28 13:47:22 +0000

    Starting re-org of src/ tree. Tests in src/ will most likely break the next couple of weeks. Tests in test/functional still work.
    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@967 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

--- test/functional/media/js/tests.js
+++ test/functional/media/js/tests.js
@@ -2,7 +2,7 @@
 // adds menu as well as stylesheet to tests
 // the only file to include in the HTML for tests is this one
 
-var init = function() {
+var _init = function() {
 	var head = document.getElementsByTagName('head')[0];
 	var css = document.createElement('link');
 	css.type = 'text/css';
@@ -19,6 +19,7 @@ var init = function() {
 };
 
 
+
 // dom loaded shorthand http://www.kryogenix.org/days/2007/09/26/shortloaded
 (function(i) {var u =navigator.userAgent;var e=/*@cc_on!@*/false; var st = 
 setTimeout;if(/webkit/i.test(u)){st(function(){var dr=document.readyState;
@@ -26,4 +27,4 @@ if(dr=="loaded"||dr=="complete"){i()}else{st(arguments.callee,10);}},10);}
 else if((/mozilla/i.test(u)&&!/(compati)/.test(u)) || (/opera/i.test(u))){
 document.addEventListener("DOMContentLoaded",i,false); } else if(e){     (
 function(){var t=document.createElement('doc:rdy');try{t.doScroll('left');
-i();t=null;}catch(e){st(arguments.callee,0);}})();}else{window.onload=i;}})(init);
+i();t=null;}catch(e){st(arguments.callee,0);}})();}else{window.onload=i;}})(_init);
