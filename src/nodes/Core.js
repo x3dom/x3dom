a5e3aba3a6358f815d31ef9cf58cf8fcb23c3e85
thyrell <thyrell@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2011-05-16 11:50:13 +0000

    Refactor Environmental Effects to nodes/EnvironmentalEffects.js
    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@1064 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

--- src/nodes/Core.js
+++ src/nodes/Core.js
@@ -4245,42 +4245,6 @@ x3dom.registerNodeType(
     )
 );
 
-/* ### X3DBackgroundNode ### */
-x3dom.registerNodeType(
-    "X3DBackgroundNode",
-    "EnvironmentalEffects",
-    defineClass(x3dom.nodeTypes.X3DBindableNode,
-        function (ctx) {
-            x3dom.nodeTypes.X3DBackgroundNode.superClass.call(this, ctx);
-
-            this._dirty = true;
-        },
-        {
-            getSkyColor: function() {
-                return new x3dom.fields.SFColor(0,0,0);
-            },
-            getTransparency: function() {
-                return 0;
-            },
-            getTexUrl: function() {
-                return [];
-            }
-        }
-    )
-);
-
-/* ### X3DFogNode ### */
-x3dom.registerNodeType(
-    "X3DFogNode",
-    "EnvironmentalEffects",
-    defineClass(x3dom.nodeTypes.X3DBindableNode,
-        function (ctx) {
-            x3dom.nodeTypes.X3DFogNode.superClass.call(this, ctx);
-        },
-        {
-        }
-    )
-);
 
 
 /* ### Viewpoint ### */
@@ -4383,23 +4347,6 @@ x3dom.registerNodeType(
     )
 );
 
-/* ### Fog ### */
-x3dom.registerNodeType(
-    "Fog",
-    "EnvironmentalEffects",
-    defineClass(x3dom.nodeTypes.X3DFogNode,
-        function (ctx) {
-            x3dom.nodeTypes.Fog.superClass.call(this, ctx);
-
-            this.addField_SFColor(ctx, 'color', 1, 1, 1);
-            this.addField_SFString(ctx, 'fogType', "LINEAR");
-            this.addField_SFFloat(ctx, 'visibilityRange', 0);
-        },
-        {
-            // methods
-        }
-    )
-);
 
 /* ### NavigationInfo ### */
 x3dom.registerNodeType(
@@ -4446,72 +4393,6 @@ x3dom.registerNodeType(
     )
 );
 
-/* ### Background ### */
-x3dom.registerNodeType(
-    "Background",
-    "EnvironmentalEffects",
-    defineClass(x3dom.nodeTypes.X3DBackgroundNode,
-        function (ctx) {
-            x3dom.nodeTypes.Background.superClass.call(this, ctx);
-
-            var trans = ctx.autoGen ? 1 : 0;
-
-            this.addField_MFColor(ctx, 'skyColor', [new x3dom.fields.SFColor(0,0,0)]);
-            this.addField_MFFloat(ctx, 'skyAngle', []);
-            this.addField_MFColor(ctx, 'groundColor', []);
-            this.addField_MFFloat(ctx, 'groundAngle', []);
-            this.addField_SFFloat(ctx, 'transparency', trans);
-            this.addField_MFString(ctx, 'backUrl', []);
-            this.addField_MFString(ctx, 'bottomUrl', []);
-            this.addField_MFString(ctx, 'frontUrl', []);
-            this.addField_MFString(ctx, 'leftUrl', []);
-            this.addField_MFString(ctx, 'rightUrl', []);
-            this.addField_MFString(ctx, 'topUrl', []);
-        },
-        {
-            fieldChanged: function(fieldName)
-            {
-                if (fieldName.indexOf("Url") > 0) {
-                    this._dirty = true;
-                }
-                else if (fieldName === "set_bind") {
-                    this.bind(this._vf.set_bind);
-                }
-            },
-
-            getSkyColor: function() {
-                return this._vf.skyColor;
-            },
-
-            getGroundColor: function() {
-                return this._vf.groundColor;
-            },
-
-            getTransparency: function() {
-                return this._vf.transparency;
-            },
-
-            getTexUrl: function() {
-                return [
-                    this._nameSpace.getURL(this._vf.backUrl[0]),
-                    this._nameSpace.getURL(this._vf.frontUrl[0]),
-                    this._nameSpace.getURL(this._vf.bottomUrl[0]),
-                    this._nameSpace.getURL(this._vf.topUrl[0]),
-                    this._nameSpace.getURL(this._vf.leftUrl[0]),
-                    this._nameSpace.getURL(this._vf.rightUrl[0])
-                    /*
-                    this._nameSpace.getURL(this._vf.rightUrl[0]),
-                    this._nameSpace.getURL(this._vf.leftUrl[0]),
-                    this._nameSpace.getURL(this._vf.topUrl[0]),
-                    this._nameSpace.getURL(this._vf.bottomUrl[0]),
-                    this._nameSpace.getURL(this._vf.frontUrl[0]),
-                    this._nameSpace.getURL(this._vf.backUrl[0])
-                    */
-                ];
-            }
-        }
-    )
-);
 
 
 /* ### X3DShapeNode ### */
