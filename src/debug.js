1e53c5c5954187e653040c8b38bc53f745418758
akustikkoppler <akustikkoppler@3fb95f9c-b211-4acc-b2b6-5f5c22209b5a>
2011-06-08 09:10:46 +0000

    git-svn-id: https://x3dom.svn.sourceforge.net/svnroot/x3dom/trunk@1157 3fb95f9c-b211-4acc-b2b6-5f5c22209b5a

--- src/debug.js
+++ src/debug.js
@@ -76,14 +76,13 @@ x3dom.debug = {
 		
 		//Need this HACK for IE/Flash integration. IE don't have a document.body at this time when starting Flash-Backend
 		if(!x3dom.debug.isAppend) {
-			alert(navigator.appName);
 			if(navigator.appName == "Microsoft Internet Explorer") {
 				//document.documentElement.appendChild(aDiv);
 				x3dom.debug.logContainer.style.marginLeft = "8px";
 				document.documentElement.appendChild(x3dom.debug.logContainer);
 			}else{
 				//document.body.appendChild(aDiv);
-				var body = document.body.appendChild(x3dom.debug.logContainer);
+				document.body.appendChild(x3dom.debug.logContainer);
 			}
 			x3dom.debug.isAppend = true;
 		}
