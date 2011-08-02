1668a0bdbc5dc6770034aa9bfae5800a90b99dda
Yvonne Jung <yjung@igd.fhg.de>
2011-08-02 17:46:06 +0200

    added time dependence

--- src/X3DCanvas.js
+++ src/X3DCanvas.js
@@ -848,25 +848,36 @@ x3dom.X3DCanvas.prototype.tick = function()
 		}
 
         if (this.statDiv || this.progressDiv) {
-            if (this.doc.lastDownloadCount !== this.doc.downloadCount)
-            {
-                if (this.statDiv) {
+            if (this.statDiv && this.doc.downloadCount) {
+                if (this.doc.needRender)
+                {
                     this.statDiv.appendChild(document.createElement("br"));
                     this.statDiv.appendChild(document.createTextNode("#Loading: " + this.doc.downloadCount));
                 }
-                if (this.progressDiv) {
-                    // TODO: In order to display a bar we need a max value to determine where we are
-                    // 100 / total * this.doc.downloadCount
-                    // this.progressDiv.childNodes[1].setAttribute("style", "width: " + progressPercent + "%");
-                    this.progressDiv.childNodes[0].textContent = 'Loading: ' + this.doc.downloadCount;
-                    if (this.doc.downloadCount > 0) {
-                        this.progressDiv.style.display = 'inline';
-                    } else {
-                        this.progressDiv.style.display = 'none';
-                    }
+                else {
+                    this.statDiv.textContent = "#Loading: " + this.doc.downloadCount;
                 }
             }
-            this.doc.lastDownloadCount = this.doc.downloadCount;
+
+            if (this.progressDiv) {
+                // TODO: In order to display a bar we need a max value to determine where we are
+                // 100 / total * this.doc.downloadCount
+                // this.progressDiv.childNodes[1].setAttribute("style", "width: " + progressPercent + "%");
+                this.progressDiv.childNodes[0].textContent = 'Loading: ' + this.doc.downloadCount;
+                if (this.doc.downloadCount > 0) {
+                    this.progressDiv.style.display = 'inline';
+                } else {
+                    this.progressDiv.style.display = 'none';
+                }
+
+                // TODO: fix these strange window-bound scope issues!
+                window.myThat = this;
+                window.myStopProgress = function stopProgress() {
+                    window.myThat.doc.downloadCount = 0;
+                    window.myThat.progressDiv.style.display = 'none';
+                };
+                window.setTimeout("window.myStopProgress()", 1500);
+            }
         }
 
     } catch (e) {
