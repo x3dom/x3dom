a616f84868b8c9b4805db152bd4b3ecb15cee0a0
Andreas Aderhold <andreas.aderhold@igd.fraunhofer.de>
2011-08-29 15:57:03 +0200

    Using default theme for readthedocs.org

--- doc/guide/conf.py
+++ doc/guide/conf.py
@@ -97,6 +97,11 @@ sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'flask'
 
+# set default theme for rtd
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
