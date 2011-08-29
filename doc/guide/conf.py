d1531e814d64dce00d3b8938ab9de1795748d79b
Andreas Aderhold <andreas.aderhold@igd.fraunhofer.de>
2011-08-29 16:37:07 +0200

    Added links to pdf and epub versions

--- doc/guide/conf.py
+++ doc/guide/conf.py
@@ -101,7 +101,9 @@ html_theme = 'flask'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
-
+    version = ''
+    # The full version, including alpha/beta/rc tags.
+    release = ''
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
