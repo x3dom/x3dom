e634efa3c5445c6d9927f03c44593d06676136a6
Andreas Aderhold <andreas.aderhold@igd.fraunhofer.de>
2011-11-29 15:07:18 +0100

    Load order fix

--- tools/packages.py
+++ tools/packages.py
@@ -2,13 +2,14 @@
 def prefix_path(lst, path):
     return [path + '/' + entry for entry in lst]
 
+# Note: order is important
 BASICS = [
   'lang/Array.js',
-  'lang/DoublyLinkedList.js',
   'Internals.js',
   'debug.js',
   'ImageLoadManager.js',
   'lang/Properties.js',
+  'lang/DoublyLinkedList.js',
   'X3DCanvas.js',
   'Runtime.js',
   'Main.js'
@@ -20,33 +21,30 @@ GFX = [
 ]
 
 NODES = [
-  'Mesh.js',
-  'X3DDocument.js',
-  'MatrixMixer.js',
-  'Viewarea.js',
-  'nodes/NodeNameSpace.js',
-  'nodes/Core.js',
-  'nodes/Grouping.js',
-  'nodes/Bindable.js',
-  'nodes/Rendering.js',
-  'nodes/Shape.js',
-  'nodes/Lighting.js',
-  'nodes/Interpolation.js',
-  'nodes/Followers.js',
-  'nodes/Time.js',
-  'nodes/Networking.js',
-  'nodes/EnvironmentalEffects.js',
-  'nodes/Navigation.js',
-  'nodes/Text.js',
-  'nodes/Sound.js',
-  'nodes/Texturing.js',
-  'nodes/Shaders.js',
-  'nodes/Geometry3D.js',
-  'nodes/Geospatial.js'
-]
-
-FIELDS = [
-  'fields.js'
+    'X3DDocument.js',
+    'MatrixMixer.js',
+    'Viewarea.js',
+    'Mesh.js',
+    'fields.js',
+    'nodes/NodeNameSpace.js',
+    'nodes/Core.js',
+    'nodes/Grouping.js',
+    'nodes/Bindable.js',
+    'nodes/Rendering.js',
+    'nodes/Shape.js',
+    'nodes/Lighting.js',
+    'nodes/Followers.js',
+    'nodes/Interpolation.js',
+    'nodes/Time.js',
+    'nodes/Networking.js',
+    'nodes/EnvironmentalEffects.js',
+    'nodes/Navigation.js',
+    'nodes/Text.js',
+    'nodes/Sound.js',
+    'nodes/Texturing.js',
+    'nodes/Shaders.js',
+    'nodes/Geometry3D.js',
+    'nodes/Geospatial.js'
 ]
 
 COMPONENTS = [
@@ -54,7 +52,7 @@ COMPONENTS = [
   'nodes/VolumeRendering.js'
 ]
 
-CORE_PROFILE = BASICS + GFX + NODES + FIELDS
+CORE_PROFILE = BASICS + GFX + NODES
 MORE_PROFILE = COMPONENTS
 
 FULL_PROFILE = CORE_PROFILE + MORE_PROFILE
