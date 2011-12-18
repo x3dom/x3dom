2b1fdc1cf13443d7ccb6603ad929d5d89273e448
tsturm <timo.sturm@igd.fraunhofer.de>
2011-12-18 03:14:53 +0100

    - little shader fix

--- src/gfx_webgl.js
+++ src/gfx_webgl.js
@@ -1249,7 +1249,7 @@ x3dom.gfx_webgl = (function () {
 		var solid				= (shape.isSolid()) ? 1 : 0;
 		var texture				= (shape._cf.appearance.node._cf.texture.node || x3dom.isa(shape._cf.geometry.node, x3dom.nodeTypes.Text)) ? 1 : 0;
 		var cubeMap				= (shape._cf.appearance.node._cf.texture.node) ? x3dom.isa(shape._cf.appearance.node._cf.texture.node, x3dom.nodeTypes.X3DEnvironmentTextureNode) ? 1 : 0 : 0;
-		var blending			= (x3dom.isa(shape._cf.geometry.node, x3dom.nodeTypes.Text) || cubeMap || (shape._cf.appearance.node._cf.texture.node && (shape._cf.appearance.node._cf.texture.node._vf.origChannelCount == 1 || shape._cf.appearance.node._cf.texture.node._vf.origChannelCount == 2))) ? 1 : 0;
+		var blending			= (x3dom.isa(shape._cf.geometry.node, x3dom.nodeTypes.Text) || cubeMap || (shape._cf.appearance.node._cf.texture.node && (shape._cf.appearance.node._cf.texture.node._vf.origChannelCount == 1.0 || shape._cf.appearance.node._cf.texture.node._vf.origChannelCount == 2.0))) ? 1 : 0;
 		var shader 				= (shape._cf.appearance.node._shader && x3dom.isa(shape._cf.appearance.node._shader, x3dom.nodeTypes.CommonSurfaceShader)) ? 1 : 0;
 		var shaderDiffuse 		= (shader && shape._cf.appearance.node._shader.getDiffuseMap()) ? 1 : 0;
 		var shaderNormal 		= (shader && shape._cf.appearance.node._shader.getNormalMap()) ? 1 : 0;
@@ -1261,7 +1261,7 @@ x3dom.gfx_webgl = (function () {
                                            fogs +
                                            lights +
                                            shadows  +
-										   solid +
+										   blending +
                                            shader +
 										   shaderDiffuse +
 										   shaderNormal +
