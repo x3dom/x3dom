75175466634ae692c8cc5fbd5d301e918bcebeb3
mlimper <max.limper@gris.tu-darmstadt.de>
2012-09-19 19:14:08 +0200

    Added pure webgl example using aopts output for bitlod vertex color loading

--- test/exploration/bitwise_refinement/bunnyBitComposerColors.js
+++ test/exploration/bitwise_refinement/bunnyBitComposerColors.js
@@ -34,7 +34,7 @@ var glBuffers;	//object data buffers on the gpu
 
 var positionAttribLocation;
 var normalAttribLocation;
-var texcoordAttributeLocation;
+var colorAttributeLocation;
 
 var refinedLevels = 0, refinementsTexCoord = 0;
 
@@ -78,7 +78,7 @@ load : function(gl)
   
   var refinementURLs = [];
   for (i = 0; i < NumLevels; ++i) {
-    refinementURLs[i] = (encodeURI('bitbunny_aopt_textured/LG_0_level' + i + '.bin'));
+    refinementURLs[i] = (encodeURI('bitbunny_aopt/LG_0_level' + i + '.bin'));
   }
   
   this.xform = new SglTransformStack();
@@ -94,19 +94,19 @@ load : function(gl)
   this.program = program;
   program.use();
   
-  positionAttribLocation    = gl.getAttribLocation(this.program.handle_, 'a_position');
-  normalAttribLocation      = gl.getAttribLocation(this.program.handle_, 'a_normal');
-  texcoordAttributeLocation = gl.getAttribLocation(this.program.handle_, 'a_texcoord');
+  positionAttribLocation = gl.getAttribLocation(this.program.handle_, 'a_position');
+  normalAttribLocation   = gl.getAttribLocation(this.program.handle_, 'a_normal');
+  colorAttributeLocation = gl.getAttribLocation(this.program.handle_, 'a_color');
   
   gl.enableVertexAttribArray(positionAttribLocation);
   gl.enableVertexAttribArray(normalAttribLocation);
-  gl.enableVertexAttribArray(texcoordAttributeLocation);
+  gl.enableVertexAttribArray(colorAttributeLocation);
   
   glBuffers = {};  
   glBuffers.indices   = gl.createBuffer();
   glBuffers.positions = gl.createBuffer();
   glBuffers.normals   = gl.createBuffer();
-  glBuffers.texcoords = gl.createBuffer();
+  glBuffers.colors    = gl.createBuffer();
   
   glContext = gl;
     
@@ -124,12 +124,12 @@ load : function(gl)
     gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindTexture(gl.TEXTURE_2D, null);
   };
-  img.src = "bitbunny_aopt_textured/fractal.png";
+  img.src = "bitbunny_aopt/fractal.png";
   //END GET TEXTURE
   
 	//BEGIN GET INDICES
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", 'bitbunny_aopt_textured/LG_0_indexBinary.bin', true);
+	xhr.open("GET", 'bitbunny_aopt/LG_0_indexBinary.bin', true);
 	xhr.responseType = "arraybuffer";            
 	xhr.send(null);
 
@@ -147,16 +147,16 @@ load : function(gl)
   
   const numVerts = 34834;
   
-  var buf = new ArrayBuffer(12 * numVerts);  
+  var buf = new ArrayBuffer(6*2 * numVerts);  
   var interleavedCoordNormalBuffer = new Uint16Array(buf);
   
-  var tBuf = new ArrayBuffer(4 * numVerts);  
-  var texCoordBuffer = new Uint16Array(tBuf);
+  var cBuf = new ArrayBuffer(3*2 * numVerts);  
+  var colorBuffer = new Uint16Array(cBuf);
   
   x3dom.RefinementJobManager.suppressOnTransferablesNotSupported = true;
   
   refinementManager.addResultBuffer(0, interleavedCoordNormalBuffer);
-  refinementManager.addResultBuffer(1, texCoordBuffer);
+  refinementManager.addResultBuffer(1, colorBuffer);
   
   //normal / coord refinements
   for (i = 0; i < NumLevels; ++i) {
@@ -172,18 +172,18 @@ load : function(gl)
                                        [0, 64]);                    //write offset (bits) information array                                       
   }
   
-  //texcoord refinements
-  texCoordURLs = ['bitbunny_aopt_textured/LG_0_level8.bin',  'bitbunny_aopt_textured/LG_0_level9.bin',
-                  'bitbunny_aopt_textured/LG_0_level10.bin', 'bitbunny_aopt_textured/LG_0_level11.bin'];
+  //color refinements
+  colorURLs = ['bitbunny_aopt/LG_0_level8.bin',  'bitbunny_aopt/LG_0_level9.bin',
+               'bitbunny_aopt/LG_0_level10.bin', 'bitbunny_aopt/LG_0_level11.bin'];
   for (i = 0; i < 4; ++i) {
     refinementManager.addRefinementJob(1,                           //attributeId / resultBufferId
                                        i,                           //download priority
-                                       texCoordURLs[i],             //data file url
+                                       colorURLs[i],                //data file url
                                        i,                           //refinement level (-> important for bit shift)
                                        refinementFinishedCallback,  //'job finished'-callback
-                                       32,                          //stride in bits (size of a single result element)
-                                       [2],                         //number of components information array
-                                       [8],                         //bits per refinement level information array
+                                       48,                          //stride in bits (size of a single result element)
+                                       [3],                         //number of components information array
+                                       [6],                         //bits per refinement level information array
                                        [0],                         //read offset (bits) information array
                                        [0]);                        //write offset (bits) information array                                       
   }
@@ -215,13 +215,13 @@ draw : function(gl)
 
     this.xform.view.loadIdentity();
     //this.xform.view.lookAt(0.0, 2.0, 3.0,
-    this.xform.view.lookAt(0.0, 0.5, 1.0,
-                           0.0, 0.0, 0.0,
+    this.xform.view.lookAt(0.0, -1.0, 2.0,
+                           0.0, -1.0, 0.0,
                            0.0, 1.0, 0.0);
 
     this.xform.model.loadIdentity();	
-    //this.xform.model.rotate(sglDegToRad(this.angle), 0.0, 1.0, 0.0);
-    this.xform.model.rotate(sglDegToRad(90.0), 1.0, 0.0, 0.0);
+    this.xform.model.rotate(sglDegToRad(this.angle), 0.0, 1.0, 0.0);
+    //this.xform.model.rotate(sglDegToRad(90.0), 1.0, 0.0, 0.0);
 
     gl.uniformMatrix4fv(this.program.set_uniform["u_mvp"], false,
                         this.xform.modelViewProjectionMatrix);
@@ -236,11 +236,11 @@ draw : function(gl)
     gl.vertexAttribPointer(positionAttribLocation, 3, gl.UNSIGNED_SHORT, true, 6*2, 0  );
     gl.vertexAttribPointer(normalAttribLocation,   2, gl.UNSIGNED_SHORT, true, 6*2, 4*2);
     
-    gl.bindBuffer(gl.ARRAY_BUFFER, glBuffers.texcoords);
-    gl.vertexAttribPointer(texcoordAttributeLocation, 2, gl.UNSIGNED_SHORT, true, 2*2, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, glBuffers.colors);
+    gl.vertexAttribPointer(colorAttributeLocation, 3, gl.UNSIGNED_SHORT, true, 3*2, 0);
 
     gl.drawElements(gl.TRIANGLES, 208353, gl.UNSIGNED_SHORT, 0);  
-  }  
+  }
 }
 };
 
@@ -260,9 +260,9 @@ function refinementFinishedCallback(attributeId, bufferView) {
     }
   }
   else if (attributeId === 1) {
-    console.log('=> Client received refined texcoord data!');
+    console.log('=> Client received refined color data!');
      
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, glBuffers.texcoords);
+    glContext.bindBuffer(glContext.ARRAY_BUFFER, glBuffers.colors);
     glContext.bufferData(glContext.ARRAY_BUFFER, bufferView, glContext.STATIC_DRAW);
   }  
   
