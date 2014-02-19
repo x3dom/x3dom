cc3310a7c8931e6060eeab0eeae2fd9cc787d8e3
mthoener <maik.thoener@igd.fraunhofer.de>
2014-02-19 17:29:54 +0100

    added webserver to batch-script, graphicsmagick still needed

--- test/regression-suite/run.bat
+++ test/regression-suite/run.bat
@@ -1,2 +1,6 @@
-START /b java -jar selenium-server-standalone-2.39.0.jar
-node.exe src/x3dom-regression.js
+START "seleniumfirefox" java -DSTOP.PORT=8079 -DSTOP.KEY=secret -jar selenium-server-standalone-2.39.0.jar
+START "nodeserv" node.exe src/server.js
+TIMEOUT 2 > nul
+node src/x3dom-regression.js
+taskkill /fi "WINDOWTITLE eq nodeserv"
+taskkill /fi "WINDOWTITLE eq seleniumfirefox"
