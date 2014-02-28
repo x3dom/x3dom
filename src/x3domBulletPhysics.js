6b98d5007be40f82ccf78bf6a6195bfcaa00fe92
astamoulias <andr.stamoulias@gmail.com>
2014-02-28 12:32:19 +0200

    Update x3domBulletPhysics.js

--- src/x3domBulletPhysics.js
+++ src/x3domBulletPhysics.js
@@ -1,31 +1,50 @@
 /*
 
-Copyright (c) 2014 held by Andreas Stamoulias, Multimedia Content Lab (MCLab) (medialab.teicrete.gr),
+Initial contribution/implementation by Andreas Stamoulias, Multimedia Content Lab (MCLab) (medialab.teicrete.gr),
 Technological Institute of Crete - Department of Informatics Engineering.
-
 
 Specific optimizations and additions were based on Don Brutzman's (http://faculty.nps.edu/brutzman/brutzman.html) commentary 
 and the X3D Rigid body physics examples (http://www.web3d.org/x3d/content/examples/Basic/RigidBodyPhysics/).
 
 This software is based on x3dom framework and ammo.js under their licenses.
 
-This software is provided 'as-is', without any express or implied
-warranty. In no event will the authors be held liable for any damages
-arising from the use of this software.
+This software is dual licensed under the MIT and GPL licenses.
 
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
+==[MIT]====================================================================
 
-1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgement in the product documentation would be
-   appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
----
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+==[GPL]====================================================================
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
