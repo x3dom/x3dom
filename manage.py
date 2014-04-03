9f86079cfb6479d2038898b933b63c33c96b2cec
bhaettasch <Benjamin.Haettasch@googlemail.com>
2014-04-03 14:47:06 +0200

    removed dumpNodeTypeTree copy operation from packaging script

--- manage.py
+++ manage.py
@@ -136,7 +136,6 @@ def build(mode='production'):
     shutil.copy('AUTHORS', DIST_ROOT)
     shutil.copy(SRC_ROOT + '/x3dom.css', DIST_ROOT)
     shutil.copy(SRC_ROOT + '/flashbackend/bin/x3dom.swf', DIST_ROOT)
-    shutil.copy(DOC_ROOT + '/help/dumpNodeTypeTree.html', DIST_ROOT + '/docs')
     shutil.copy(SRC_ROOT + '/dash.all.js', DIST_ROOT)
     shutil.copy(SRC_ROOT + '/ammo.js', DIST_ROOT)
     shutil.copy(SRC_ROOT + '/x3domBulletPhysics.js', DIST_ROOT)
