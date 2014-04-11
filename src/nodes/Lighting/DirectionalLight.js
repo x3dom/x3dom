c3993402a7115733559e96788e04ebef63b2a029
mthoener <maik.thoener@igd.fraunhofer.de>
2014-04-11 17:07:14 +0200

    comments for doc

--- src/nodes/Lighting/DirectionalLight.js
+++ src/nodes/Lighting/DirectionalLight.js
@@ -16,18 +16,23 @@ x3dom.registerNodeType(
         /**
          * Constructor for DirectionalLight
          * @constructs x3dom.nodeTypes.DirectionalLight
-         * @x3d x.x
+         * @x3d 3.0
          * @component Lighting
          * @status experimental
          * @extends x3dom.nodeTypes.X3DLightNode
          * @param {Object} [ctx=null] - context object, containing initial settings like namespace
+         * @classdesc The DirectionalLight node defines a directional light source that illuminates along rays parallel to a given 3-dimensional vector.
+         * A directional light source illuminates only the objects in its enclosing parent group.
+         * The light may illuminate everything within this coordinate system, including all children and descendants of its parent group.
+         * The accumulated transformations of the parent nodes affect the light.
+         * DirectionalLight nodes do not attenuate with distance.
          */
         function (ctx) {
             x3dom.nodeTypes.DirectionalLight.superClass.call(this, ctx);
 
 
             /**
-             *
+             * The direction field specifies the direction vector of the illumination emanating from the light source in the local coordinate system.
              * @var {SFVec3f} direction
              * @memberof x3dom.nodeTypes.DirectionalLight
              * @initvalue 0,0,-1
--- src/nodes/Lighting/PointLight.js
+++ src/nodes/Lighting/PointLight.js
@@ -16,42 +16,50 @@ x3dom.registerNodeType(
         /**
          * Constructor for PointLight
          * @constructs x3dom.nodeTypes.PointLight
-         * @x3d x.x
+         * @x3d 3.0
          * @component Lighting
-         * @status experimental
+         * @status full
          * @extends x3dom.nodeTypes.X3DLightNode
          * @param {Object} [ctx=null] - context object, containing initial settings like namespace
+         * @classdesc The PointLight node specifies a point light source at a 3D location in the local coordinate system.
+         * A point light source emits light equally in all directions; that is, it is omnidirectional.
+         * PointLight nodes are specified in the local coordinate system and are affected by ancestor transformations.
          */
         function (ctx) {
             x3dom.nodeTypes.PointLight.superClass.call(this, ctx);
 
 
             /**
-             *
+             * PointLight node's illumination falls off with distance as specified by three attenuation coefficients. The attenuation factor is:
+             * 1/max(attenuation[0] + attenuation[1] × r + attenuation[2] × r^2, 1)
+             * where r is the distance from the light to the surface being illuminated.
+             * The default is no attenuation.
+             * An attenuation value of (0, 0, 0) is identical to (1, 0, 0). Attenuation values shall be greater than or equal to zero.
              * @var {SFVec3f} attenuation
              * @memberof x3dom.nodeTypes.PointLight
              * @initvalue 1,0,0
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_SFVec3f(ctx, 'attenuation', 1, 0, 0);
 
             /**
-             *
+             * The position of the Light
              * @var {SFVec3f} location
              * @memberof x3dom.nodeTypes.PointLight
              * @initvalue 0,0,0
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_SFVec3f(ctx, 'location', 0, 0, 0);
 
             /**
-             *
+             * A PointLight node illuminates geometry within radius length base units of its location.
+             * Radius is affected by acnestors' transformations.
              * @var {SFFloat} radius
              * @memberof x3dom.nodeTypes.PointLight
              * @initvalue 100
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_SFFloat(ctx, 'radius', 100);
