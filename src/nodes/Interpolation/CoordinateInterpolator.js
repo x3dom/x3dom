7245fbc618035e2ea643086cd84e7896ad049116
mthoener <maik.thoener@igd.fraunhofer.de>
2014-04-16 15:33:59 +0200

    doc comments

--- src/nodes/Interpolation/CoordinateInterpolator.js
+++ src/nodes/Interpolation/CoordinateInterpolator.js
@@ -16,22 +16,25 @@ x3dom.registerNodeType(
         /**
          * Constructor for CoordinateInterpolator
          * @constructs x3dom.nodeTypes.CoordinateInterpolator
-         * @x3d x.x
+         * @x3d 3.0
          * @component Interpolation
-         * @status experimental
+         * @status full
          * @extends x3dom.nodeTypes.X3DInterpolatorNode
          * @param {Object} [ctx=null] - context object, containing initial settings like namespace
+         * @classdesc The CoordinateInterpolator node linearly interpolates among a list of MFVec3f values to produce an MFVec3f value_changed event.
+         * The number of coordinates in the keyValue field shall be an integer multiple of the number of key frames in the key field.
+         * That integer multiple defines how many coordinates will be contained in the value_changed events.
          */
         function (ctx) {
             x3dom.nodeTypes.CoordinateInterpolator.superClass.call(this, ctx);
 
 
             /**
-             *
+             * Defines the set of data points, that are used for interpolation.
              * @var {MFVec3f} keyValue
              * @memberof x3dom.nodeTypes.CoordinateInterpolator
              * @initvalue []
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_MFVec3f(ctx, 'keyValue', []);
