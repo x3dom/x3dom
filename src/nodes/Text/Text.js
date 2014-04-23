e29a6f78c492229a36b1beed6df1593b5b052cc9
mthoener <maik.thoener@igd.fraunhofer.de>
2014-04-23 16:25:07 +0200

    doc comments

--- src/nodes/Text/Text.js
+++ src/nodes/Text/Text.js
@@ -16,52 +16,59 @@ x3dom.registerNodeType(
         /**
          * Constructor for Text
          * @constructs x3dom.nodeTypes.Text
-         * @x3d x.x
+         * @x3d 3.3
          * @component Text
          * @status experimental
          * @extends x3dom.nodeTypes.X3DGeometryNode
          * @param {Object} [ctx=null] - context object, containing initial settings like namespace
+         * @classdesc The Text node specifies a two-sided, flat text string object positioned in the Z=0 plane of the local coordinate system based on values defined in the fontStyle field.
+         * Text nodes may contain multiple text strings specified using the UTF-8 encoding.
+         * The text strings are stored in the order in which the text mode characters are to be produced as defined by the parameters in the FontStyle node.
          */
         function (ctx) {
             x3dom.nodeTypes.Text.superClass.call(this, ctx);
 
 
             /**
-             *
+             * The text strings are contained in the string field.
              * @var {x3dom.fields.MFString} string
              * @memberof x3dom.nodeTypes.Text
              * @initvalue []
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_MFString(ctx, 'string', []);
 
             /**
-             *
+             * The length field contains an MFFloat value that specifies the length of each text string in the local coordinate system.
+             * The length of each line of type is measured horizontally for horizontal text (FontStyle node: horizontal=TRUE) and vertically for vertical text (FontStyle node: horizontal=FALSE).
              * @var {x3dom.fields.MFFloat} length
+             * @range [0, inf]
              * @memberof x3dom.nodeTypes.Text
              * @initvalue []
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_MFFloat(ctx, 'length', []);
 
             /**
-             *
+             * The maxExtent field limits and compresses all of the text strings if the length of the maximum string is longer than the maximum extent, as measured in the local coordinate system. If the text string with the maximum length is shorter than the maxExtent, then there is no compressing.
+             * The maximum extent is measured horizontally for horizontal text (FontStyle node: horizontal=TRUE) and vertically for vertical text (FontStyle node: horizontal=FALSE).
              * @var {x3dom.fields.SFFloat} maxExtent
+             * @range [0, inf]
              * @memberof x3dom.nodeTypes.Text
              * @initvalue 0.0
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_SFFloat(ctx, 'maxExtent', 0.0);
 
             /**
-             *
+             * The fontStyle field contains one FontStyle node that specifies the font size, font family and style, direction of the text strings, and any specific language rendering techniques used for the text.
              * @var {x3dom.fields.SFNode} fontStyle
              * @memberof x3dom.nodeTypes.Text
              * @initvalue x3dom.nodeTypes.X3DFontStyleNode
-             * @field x3dom
+             * @field x3d
              * @instance
              */
             this.addField_SFNode ('fontStyle', x3dom.nodeTypes.X3DFontStyleNode);
