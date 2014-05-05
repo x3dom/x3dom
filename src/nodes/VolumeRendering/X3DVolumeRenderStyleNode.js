2b795027a43e5da5549b29b9e06138e4382b0553
Ander Arbelaiz <arbelaitz@gmail.com>
2014-05-05 14:56:42 +0200

    Changed license header, as it was before.

--- src/nodes/VolumeRendering/X3DVolumeRenderStyleNode.js
+++ src/nodes/VolumeRendering/X3DVolumeRenderStyleNode.js
@@ -1,10 +1,14 @@
 /** @namespace x3dom.nodeTypes */
 /*
- * X3DOM JavaScript Library
- * http://www.x3dom.org
+ * MEDX3DOM JavaScript Library
+ * http://medx3dom.org
  *
- * (C)2009 Fraunhofer IGD, Darmstadt, Germany
- * Dual licensed under the MIT and GPL
+ * (C)2011 Vicomtech Research Center,
+ *         Donostia - San Sebastian
+ * Dual licensed under the MIT and GPL.
+ *
+ * Based on code originally provided by
+ * http://www.x3dom.org
  */
 
 /* ### X3DVolumeRenderStyleNode ### */
