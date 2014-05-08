97e90f83955021ad89bfbee7b912bc056c5b460d
chrstein <chrstein@gmx.net>
2014-05-08 13:55:13 +0200

    fix

--- src/NodeNameSpace.js
+++ src/NodeNameSpace.js
@@ -139,7 +139,13 @@ x3dom.getFieldValue = function(fieldName)
 x3dom.setFieldValue = function(fieldName, fieldvalue) {
     var x3dNode = this._x3domNode;
     if (x3dNode && x3dNode._vf[fieldName]) {
-        x3dNode._vf[fieldName] = fieldvalue.copy();
+
+        // SF/MF object types are cloned based on a copy function
+        if(fieldvalue instanceof  Object)
+            x3dNode._vf[fieldName] = fieldvalue.copy();
+        else
+            x3dNode._vf[fieldName] = fieldvalue;
+
         x3dNode.fieldChanged(fieldName);
         x3dNode._nameSpace.doc.needRender = true;
     }
