7575639fc989c2f308be22ffc231feceb2516771
andreasplesch <andreasplesch@netscape.net>
2014-07-21 17:29:17 -0400

    borrow framework from Viewpoint.js

--- src/nodes/Geospatial/GeoViewpoint.js
+++ src/nodes/Geospatial/GeoViewpoint.js
@@ -65,7 +65,17 @@ x3dom.registerNodeType(
             this.addField_SFRotation(ctx, 'orientation', 0, 0, 1, 0);
 
             /**
-             * The position fields of the Viewpoint node specifies a relative location in the local coordinate system. Position is relative to the coordinate system's origin (0,0,0),
+             * The centerOfRotation field specifies a center about which to rotate the user's eyepoint when in EXAMINE mode. It is provided in a format consistent with that specified by geoSystem.
+             * @var {x3dom.fields.SFVec3f} centerOfRotation
+             * @memberof x3dom.nodeTypes.GeoViewpoint
+             * @initvalue 0,0,0
+             * @field x3d
+             * @instance
+             */
+            this.addField_SFVec3f(ctx, 'centerOfRotation', 0, 0, 0);
+
+            /**
+             * The position fields of the Viewpoint node specifies a relative location in the local coordinate system. It is provided in a format consistent with that specified by geoSystem. 
              * @var {x3dom.fields.SFVec3d} position
              * @memberof x3dom.nodeTypes.GeoViewpoint
              * @initvalue 0,0,100000
@@ -114,7 +124,172 @@ x3dom.registerNodeType(
              * @instance
              */
             this.addField_SFNode('geoOrigin', x3dom.nodeTypes.X3DViewpointNode);
+
+            // borrowed from Viewpoint.js
+           
+            this._viewMatrix = x3dom.fields.SFMatrix4f.translation(this._vf.position).
+                mult(this._vf.orientation.toMatrix()).inverse();
+
+            this._projMatrix = null;
+            this._lastAspect = 1.0;
+
+            // z-ratio: a value around 5000 would be better...
+            this._zRatio = 10000;
+            this._zNear = this._vf.zNear;
+            this._zFar = this._vf.zFar;
+
+            // special stuff...
+            this._imgPlaneHeightAtDistOne = 2.0 * Math.tan(this._vf.fieldOfView / 2.0);
         
+        },
+        // all borrowed from Viewpoint.js
+        {
+            fieldChanged: function (fieldName) {
+                if (fieldName == "position" || fieldName == "orientation") {
+                    this.resetView();
+                }
+                else if (fieldName == "fieldOfView" ||
+                    fieldName == "zNear" || fieldName == "zFar") {
+                    this._projMatrix = null;   // only trigger refresh
+                    this._zNear = this._vf.zNear;
+                    this._zFar = this._vf.zFar;
+                    this._imgPlaneHeightAtDistOne = 2.0 * Math.tan(this._vf.fieldOfView / 2.0);
+                }
+                else if (fieldName.indexOf("bind") >= 0) {
+                    // FIXME; call parent.fieldChanged();
+                    this.bind(this._vf.bind);
+                }
+            },
+
+            setProjectionMatrix: function(matrix)
+            {
+                this._projMatrix = matrix;
+            },
+
+            getCenterOfRotation: function() {
+                return this._vf.centerOfRotation;
+            },
+
+            getViewMatrix: function() {
+                return this._viewMatrix;
+            },
+
+            getFieldOfView: function() {
+                return this._vf.fieldOfView;
+            },
+
+            resetView: function() {
+                this._viewMatrix = x3dom.fields.SFMatrix4f.translation(this._vf.position).
+                    mult(this._vf.orientation.toMatrix()).inverse();
+            },
+
+            getNear: function() {
+                return this._zNear;
+            },
+
+            getFar: function() {
+                return this._zFar;
+            },
+
+            getImgPlaneHeightAtDistOne: function() {
+                return this._imgPlaneHeightAtDistOne;
+            },
+
+            getProjectionMatrix: function(aspect)
+            {
+                var fovy = this._vf.fieldOfView;
+                var zfar = this._vf.zFar;
+                var znear = this._vf.zNear;
+
+                if (znear <= 0 || zfar <= 0)
+                {
+                    var nearScale = 0.8, farScale = 1.2;
+                    var viewarea = this._nameSpace.doc._viewarea;
+                    var scene = viewarea._scene;
+
+                    // Doesn't work if called e.g. from RenderedTexture with different sub-scene
+                    var min = x3dom.fields.SFVec3f.copy(scene._lastMin);
+                    var max = x3dom.fields.SFVec3f.copy(scene._lastMax);
+
+                    var dia = max.subtract(min);
+                    var sRad = dia.length() / 2;
+
+                    var mat = viewarea.getViewMatrix().inverse();
+                    var vp = mat.e3();
+
+                    // account for scales around the viewpoint
+                    var translation = new x3dom.fields.SFVec3f(0,0,0),
+                        scaleFactor = new x3dom.fields.SFVec3f(1,1,1);
+                    var rotation = new x3dom.fields.Quaternion(0,0,1,0),
+                        scaleOrientation = new x3dom.fields.Quaternion(0,0,1,0);
+
+                    // unfortunately, decompose is a rather expensive operation
+                    mat.getTransform(translation, rotation, scaleFactor, scaleOrientation);
+
+                    var minScal = scaleFactor.x, maxScal = scaleFactor.x;
+
+                    if (maxScal < scaleFactor.y) maxScal = scaleFactor.y;
+                    if (minScal > scaleFactor.y) minScal = scaleFactor.y;
+                    if (maxScal < scaleFactor.z) maxScal = scaleFactor.z;
+                    if (minScal > scaleFactor.z) minScal = scaleFactor.z;
+
+                    if (maxScal > 1)
+                        nearScale /= maxScal;
+                    else if (minScal > x3dom.fields.Eps && minScal < 1)
+                        farScale /= minScal;
+                    // near/far scale adaption done
+
+                    var sCenter = min.add(dia.multiply(0.5));
+                    var vDist = (vp.subtract(sCenter)).length();
+
+                    if (sRad) {
+                        if (vDist > sRad)
+                            znear = (vDist - sRad) * nearScale;  // Camera outside scene
+                        else
+                            znear = 0;                           // Camera inside scene
+
+                        zfar = (vDist + sRad) * farScale;
+                    }
+                    else {
+                        znear = 0.1;
+                        zfar = 100000;
+                    }
+
+                    var zNearLimit = zfar / this._zRatio;
+                    znear = Math.max(znear, Math.max(x3dom.fields.Eps, zNearLimit));
+
+                    if (zfar > this._vf.zNear && this._vf.zNear > 0)
+                        znear = this._vf.zNear;
+                    if (this._vf.zFar > znear)
+                        zfar = this._vf.zFar;
+
+                    if (zfar <= znear)
+                        zfar = znear + 1;
+                    //x3dom.debug.logInfo("near: " + znear + " -> far:" + zfar);
+                }
+
+                if (this._projMatrix == null)
+                {
+                    this._projMatrix = x3dom.fields.SFMatrix4f.perspective(fovy, aspect, znear, zfar);
+                }
+                else if (this._zNear != znear || this._zFar != zfar)
+                {
+                    var div = znear - zfar;
+                    this._projMatrix._22 = (znear + zfar) / div;
+                    this._projMatrix._23 = 2 * znear * zfar / div;
+                }
+                else if (this._lastAspect != aspect)
+                {
+                    this._projMatrix._00 = (1 / Math.tan(fovy / 2)) / aspect;
+                    this._lastAspect = aspect;
+                }
+
+                // also needed for being able to ask for near and far
+                this._zNear = znear;
+                this._zFar = zfar;
+
+                return this._projMatrix;
+            }
         }
     )
 );
