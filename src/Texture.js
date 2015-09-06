89733ff049bb4f392c119295693cae5f07193b13
Andreas Plesch <andreasplesch@users.noreply.github.com>
2015-09-06 16:21:01 -0400

    Update Texture.js

--- src/Texture.js
+++ src/Texture.js
@@ -502,7 +502,7 @@ x3dom.Texture.prototype.updateText = function()
 
 	switch(textAlignment) {
 		case "left": 	textX = 0; 						break;
-		case "center": 	textX = text_canvas.width/2; 	break;
+		case "center": 	textX = (text_canvas.width/2)/oversample; 	break; //needs to be in unscaled units
 		case "right": 	textX = text_canvas.width;		break;
 	}
 
