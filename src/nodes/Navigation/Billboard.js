7cb03d8ed1b86927243afd50b00dee70c6140785
Max Limper <max.limper@igd.fraunhofer.de>
2015-11-02 22:47:57 +0100

    Update Billboard.js

--- src/nodes/Navigation/Billboard.js
+++ src/nodes/Navigation/Billboard.js
@@ -55,7 +55,7 @@ x3dom.registerNodeType(
                     this.invalidateCache();
 
                 planeMask = drawableCollection.cull(transform, this.graphState(), singlePath, planeMask);
-                if (planeMask <= 0) {
+                if (planeMask < 0) {
                     return;
                 }
 
