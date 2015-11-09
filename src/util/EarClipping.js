b2daf40c91f4fe1aef7095cb36ee48326d44efcb
Andreas Plesch <andreasplesch@users.noreply.github.com>
2015-11-08 19:09:45 -0500

    Update EarClipping.js

--- src/util/EarClipping.js
+++ src/util/EarClipping.js
@@ -79,13 +79,13 @@ x3dom.EarClipping = {
 					isEar = false;
 				}
 			}
-			console.log(isEar,node.prev.point_index, node.point_index, node.next.point_index);
+			x3dom.debug.logInfo(isEar,node.prev.point_index, node.point_index, node.next.point_index);
 			if(isEar) {
 				if(this.isKonvex(node.prev.point, node.point, node.next.point, plane)) {
 					indexes.push(node.prev.point_index, node.point_index, node.next.point_index);
 					linklist.deleteNode(node);
 				} else {
-					console.log("not ear");
+					x3dom.debug.logInfo("not ear");
 					count++;
 				}
 			}
