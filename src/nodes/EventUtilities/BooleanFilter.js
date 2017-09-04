779aa8ed21e6a5187866e0940fc18239cc81bbb6
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-09-03 23:50:40 -0400

    filtered output

--- src/nodes/EventUtilities/BooleanFilter.js
+++ src/nodes/EventUtilities/BooleanFilter.js
@@ -68,7 +68,22 @@ x3dom.registerNodeType(
             this.addField_SFBool(ctx, 'inputNegate');
         },
         {
-            fieldChanged: function(field) {}
+            fieldChanged: function(fieldName)
+            {
+                if (fieldName === 'set_boolean') { //ignore attempted input to all other fields
+                    var input = this._vf.set_boolean;
+                    this._vf.inputNegate = !input;
+                    this.postMessage('inputNegate', !input);
+                    if (input) {
+                        this._vf.inputTrue = true;
+                        this.postMessage('inputTrue', true);
+                        return;
+                    }
+                    this._vf.inputFalse = false; // according to tooltips but cobweb uses true
+                    this.postMessage('inputFalse', false);
+                    return;
+                }
+            }
         }
     )
 );
