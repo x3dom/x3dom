f1fcb65d82b5bfb07a82e91d45d911913118088a
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-10-12 20:58:39 -0400

    extend X3DTriggerNode

--- src/nodes/EventUtilities/BooleanTrigger.js
+++ src/nodes/EventUtilities/BooleanTrigger.js
@@ -11,7 +11,7 @@
 x3dom.registerNodeType(
     "BooleanTrigger",
     "EventUtilities",
-    defineClass(x3dom.nodeTypes.X3DChildNode,
+    defineClass(x3dom.nodeTypes.X3DTriggerNode,
         
         /**
          * Constructor for BooleanTrigger
