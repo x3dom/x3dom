fa582f2be6c30f4031453a4a8e38e8c4147228ea
John Carlson <yottzumm@gmail.com>
2018-04-16 15:06:54 -0400

    changed for MFVEc3f

--- src/fields.js
+++ src/fields.js
@@ -2914,7 +2914,7 @@ x3dom.fields.MFVec3f.parse = function(str) {
 
 x3dom.fields.MFVec3f.prototype.copy = function()
 {
-    x3dom.fields.MFVec3f.copy(this);
+    return x3dom.fields.MFVec3f.copy(this);
 };
 
 x3dom.fields.MFVec3f.prototype.setValueByStr = function(str) {
@@ -2937,6 +2937,14 @@ x3dom.fields.MFVec3f.prototype.toGL = function() {
     return a;
 };
 
+x3dom.fields.MFVec3f.prototype.toString = function () {
+   var str = "";
+
+   this.forEach(function (sf) {
+   	str = str + sf.toString() + " ";
+   });
+   return str.trim();
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 /** MFVec2f constructor.
