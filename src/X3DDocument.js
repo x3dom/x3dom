d55b44ccb5fb6c27109c5a30635c978e5f3736c8
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-05-26 20:08:55 -0400

    handle scene node removal

--- src/X3DDocument.js
+++ src/X3DDocument.js
@@ -240,6 +240,9 @@ x3dom.X3DDocument.prototype._setup = function (sceneDoc, uriDocs, sceneElemPos) 
                     fromNode.removeRoute(domNode.getAttribute('fromField'), toNode, domNode.getAttribute('toField'));
                 }
             }
+            else if (domNode.localName && domNode.localName.toUpperCase() == "SCENE") {
+                removeX3DOMBackendGraph(domNode._xmlNode);
+            }
             else if (domNode.localName && domNode.localName.toUpperCase() == "X3D") {
                 var runtime = domNode.runtime;
 
