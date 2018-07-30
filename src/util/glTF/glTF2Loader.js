52d1b37a4c7ff8491ffc74c0a8c358471d15fe4d
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-07-30 13:29:45 -0400

    add interpolation to dom node

--- src/util/glTF/glTF2Loader.js
+++ src/util/glTF/glTF2Loader.js
@@ -119,6 +119,8 @@ x3dom.glTF2Loader.prototype._generateX3DAnimation = function(parent, duration, s
         '</' + interpolator + '>';
 
     var interNode = _domFromString(inter);
+    interNode.interpolation=sampler.interpolation;
+    
     var input_accessor_dom = this._generateX3DBufferAccessor('SAMPLER_INPUT', input_accessor, input_accessor.bufferView); 
     input_accessor_dom.duration = duration;
 
