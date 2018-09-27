2793acb6a6b2bd8474c123a811b77ab39e200bf3
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-09-27 13:23:04 -0400

    reuse methods

--- src/nodes/NURBS/NurbsOrientationInterpolator.js
+++ src/nodes/NURBS/NurbsOrientationInterpolator.js
@@ -120,64 +120,68 @@ x3dom.registerNodeType(
                 this.points = this._cf.controlPoint.node._vf.point; 
                 var points = this.points.length;
                 var knot = this._vf.knot;
-                if (knot.length !== points + this._vf.order) this.createDefaultKnots();
-                if (this._vf.weight.length != points) this._vf.weight = Array(points).fill(1.0);
+                if (knot.length !== points + this._vf.order)
+                    x3dom.nodeTypes.NurbsPositionInterpolator.prototype.createDefaultKnots.call(this);
+                if (this._vf.weight.length != points)
+                    this._vf.weight = Array(points).fill(1.0);
                 var uShift = (knot[knot.length-1] - knot[0]) * this._fractionalShift;
                 var diff = this.curvePoint(u).subtract(this.curvePoint(u+uShift));
                 return x3dom.fields.Quaternion.rotateFromTo(this._downZ, diff);
             },
-            createDefaultKnots: function () {
-                var points = this.points.length;
-                var knots = Array(points + this._vf.order).fill(0);
-                for (var k = this._vf.order;
-                    k < points; k++)
-                    knots[k] = (k-1) / (points-1);
-                for (var k = knots.length - this._vf.order;
-                    k < knots.length; k++)
-                    knots[k] = 1; //points-1;
-                this._vf.knot = knots;
-            },
+//             createDefaultKnots: function () {
+//                 var points = this.points.length;
+//                 var knots = Array(points + this._vf.order).fill(0);
+//                 for (var k = this._vf.order;
+//                     k < points; k++)
+//                     knots[k] = (k-1) / (points-1);
+//                 for (var k = knots.length - this._vf.order;
+//                     k < knots.length; k++)
+//                     knots[k] = 1; //points-1;
+//                 this._vf.knot = knots;
+//             },
             curvePoint: function (u) {
-                var nurb = {
-                    dimension: this.points.length-1,
-                    degree: this._vf.order-1,
-                    knots: this._vf.knot,
-                    points: this.points,
-                    weights: this._vf.weight
-                };
-               return x3dom.nodeTypes.NurbsCurve.prototype.curvePoint3DH.call(
-                this, nurb.dimension, nurb.degree, nurb.knots, nurb.points, nurb.weights, u);    
+                return x3dom.nodeTypes.NurbsPositionInterpolator.prototype.curvePoint.call(this, u);
+//                 var nurb = {
+//                     dimension: this.points.length-1,
+//                     degree: this._vf.order-1,
+//                     knots: this._vf.knot,
+//                     points: this.points,
+//                     weights: this._vf.weight
+//                 };
+//                return x3dom.nodeTypes.NurbsCurve.prototype.curvePoint3DH.call(
+//                 this, nurb.dimension, nurb.degree, nurb.knots, nurb.points, nurb.weights, u);    
             },
             findSpan: function (n, p, u, U) {
                 return x3dom.nodeTypes.NurbsCurve.prototype.findSpan(n, p, u, U);
             }, /* findSpan */
             basisFuns: function (i, u, p, U) { // modified to disable cache
+                return x3dom.nodeTypes.NurbsPositionInterpolator.prototype.basisFuns(i, u, p, U);
                 //var uKey = Math.floor(u*10e10);
                 //if (this.basisFunsCache[uKey]) return this.basisFunsCache[uKey];
-                var N = [], left = [], right = [], saved, temp;
-                var j, r;
+//                 var N = [], left = [], right = [], saved, temp;
+//                 var j, r;
 
-                N[0] = 1.0;
-                for(j = 0; j <= p; j++) {
-                    left[j] = 0;
-                    right[j] = 0;
-                }
+//                 N[0] = 1.0;
+//                 for(j = 0; j <= p; j++) {
+//                     left[j] = 0;
+//                     right[j] = 0;
+//                 }
                 
-                for(j = 1; j <= p; j++)	{
-                    left[j] = u - U[i+1-j];
-                    right[j] = U[i+j] - u;
-                    saved = 0.0;
+//                 for(j = 1; j <= p; j++)	{
+//                     left[j] = u - U[i+1-j];
+//                     right[j] = U[i+j] - u;
+//                     saved = 0.0;
 
-                    for(r = 0; r < j; r++) {
-                        temp = N[r] / (right[r+1] + left[j-r]);
-                        N[r] = saved + right[r+1] * temp;
-                        saved = left[j-r] * temp;
-                    }
+//                     for(r = 0; r < j; r++) {
+//                         temp = N[r] / (right[r+1] + left[j-r]);
+//                         N[r] = saved + right[r+1] * temp;
+//                         saved = left[j-r] * temp;
+//                     }
 
-                    N[j] = saved;
-                }
-                //this.basisFunsCache[uKey] = N;
-                return N;
+//                     N[j] = saved;
+//                 }
+//                 //this.basisFunsCache[uKey] = N;
+//                 return N;
             } /* basisFuns */
         }
     )
