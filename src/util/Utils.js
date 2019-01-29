eb92bc5a4a84b1f69dc3f4b22e64cd2fe943737c
Andreas Plesch <andreasplesch@users.noreply.github.com>
2019-01-29 10:08:50 -0500

    dirty lighting by full array
    rather than count, to allow for simultaneous adding and removing

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -720,7 +720,7 @@ x3dom.Utils.isUnsignedType = function (str)
 *****************************************************************************/
 x3dom.Utils.checkDirtyLighting = function(viewarea)
 {
-	return (viewarea.getLights().length + viewarea._scene.getNavigationInfo()._vf.headlight);
+	return (viewarea.getLights().concat(viewarea._scene.getNavigationInfo()._vf.headlight));
 };
 
 /*****************************************************************************
