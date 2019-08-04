9529b88f22111a5dc6df7504af1c68a81bcc9ce2
Andreas Plesch <andreasplesch@users.noreply.github.com>
2019-08-04 15:24:47 -0400

    use dds order for background
    Since the dds background shader is used also for the case of six individual background face textures, we also need to the dds order for creating the texture cube.

--- src/gfx_webgl.js
+++ src/gfx_webgl.js
@@ -898,7 +898,7 @@ x3dom.gfx_webgl = (function() {
                 } else {
                     bgnd._webgl.shader = this.cache.getShader(gl, x3dom.shader.BACKGROUND_CUBETEXTURE_DDS);
                     bgnd._webgl.texture = x3dom.Utils.createTextureCube(gl, bgnd._nameSpace.doc, url,
-                        true, bgnd._vf.crossOrigin, true, false);
+                        false, bgnd._vf.crossOrigin, true, false);
                 }
 
 
