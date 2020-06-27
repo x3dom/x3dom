239116604663631ced58ce71c9ff6264daf7da89
FrankBuchholz <Frank.Buchholz@web.de>
2020-06-28 00:13:04 +0200

    Extend log messages
    It is not useful to show the count of explicit plus implicit colors or angels - the user would not recognize and understand these numbers. However, it might help to explain how to correct the data.
    This condition has to be true:
    (skyAngle.lenght == skyColor.length - 1) && (groundAngle.length == groundColor.length -1)

--- src/gfx_webgl.js
+++ src/gfx_webgl.js
@@ -1049,9 +1049,9 @@ x3dom.gfx_webgl = ( function ()
 
                 if ( sky.length != colors.length )
                 {
-                    x3dom.debug.logError( "Number of background colors (" + colors.length
-                        + ") and corresponding angles (" + sky.length
-                        + ") are different!" );
+					// 
+                    x3dom.debug.logError( "Number of background colors and corresponding angles do not match.\n"
+						+ "You have to define one angle less than the count of RGB colors because the angle 0° is added automatically." );
                     var minArrayLength = ( sky.length < colors.length ) ? sky.length : colors.length;
                     sky.length = minArrayLength;
                     colors.length = minArrayLength;
