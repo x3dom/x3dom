35d7babfec26bcf56103e4334ce779ff946259e0
Andreas Plesch <andreasplesch@users.noreply.github.com>
2023-05-24 11:10:22 -0400

    lint2

--- src/Docs.js
+++ src/Docs.js
@@ -169,29 +169,32 @@ x3dom.docs.getComponentInfo = function ()
                 x3dom.docs.specBaseURL + x3dom.docs.specURLMap[ c ] + "#" + t +
                 "' style='color:black; text-decoration:none; font-weight:bold;'>" +
                 t + "</a> <ul>";
-            if (! t.startsWith('X3D'))
-            {            
-                try {
-                    var node = new x3dom.nodeTypes[t];
-                    result += "-- basic fields --"
+            if ( ! t.startsWith( "X3D" ) )
+            {
+                try
+                {
+                    var node = new x3dom.nodeTypes[ t ]();
+                    result += "-- basic fields --";
                     for ( var field in node._vf )
                     {
-                        result += "<li>" + field +": " + node._vf[ field ] ;
+                        result += "<li>" + field + ": " + node._vf[ field ] ;
                         result += "</li>";
                     }
-                } catch(m) {};
-                try {    
-                    result += "-- node fields --"
+                }
+                catch ( m ) {};
+                try
+                {
+                    result += "-- node fields --";
                     for ( var cfield in node._cf )
                     {
-                        result += "<li>" + cfield +": " + JSON.stringify(node._cf[ cfield ]) ;
+                        result += "<li>" + cfield + ": " + JSON.stringify( node._cf[ cfield ] ) ;
                         result += "</li>";
                     }
-                    
-                } catch(m) {};
+                }
+                catch ( m ) {};
             }
             result += "</ul> </li>";
-    }
+        }
         result += "</ul>";
     }
 
