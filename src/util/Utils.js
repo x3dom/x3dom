ce26b246dfd80ac80af41540641bb1e5abcc1637
Andreas Plesch <andreasplesch@users.noreply.github.com>
2023-09-21 03:05:23 +0000

    colorSpaceConversion field

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -166,6 +166,10 @@ x3dom.Utils.createTexture2D = function ( gl, doc, src, bgnd, crossOrigin, scale,
             gl.pixelStorei( gl.UNPACK_FLIP_Y_WEBGL, true );
         }
         gl.bindTexture( gl.TEXTURE_2D, texture );
+        if ( tex && tex._vf.colorSpaceConversion == false )
+        {
+            gl.pixelStorei( gl.UNPACK_COLORSPACE_CONVERSION_WEBGL, gl.NONE );	
+        }
         //gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
         gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image );
         if ( genMipMaps )
