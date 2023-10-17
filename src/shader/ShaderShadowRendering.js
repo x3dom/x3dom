0707c6109565b2898381b23ec91e764694c10cb2
dmorehead <mrmernan@yahoo.com>
2023-10-16 19:46:24 -0700

    fog w shadows & ssao

--- src/shader/ShaderShadowRendering.js
+++ src/shader/ShaderShadowRendering.js
@@ -172,7 +172,6 @@ x3dom.shader.ShadowRenderingShader.prototype.generateFragmentShader = function (
 
     if ( properties.FOG )
     {
-    // mask shadow w/ fogDepth, visually no need for fogColor
         shader += "   vec3 fragEyePosition = eyeCoords.xyz / eyeCoords.w;\n" +
               "   float f0 = calcFog(fragEyePosition);\n" +
               "   color.rgb = vec3(1.0, 1.0, 1.0) * (1.0 - f0) + f0 * color.rgb;\n";
