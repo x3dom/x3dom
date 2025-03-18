7362a11308334117e4ccdcadd0b9ab8c8fa594c4
Andreas Plesch <andreasplesch@users.noreply.github.com>
2025-03-17 23:10:18 -0400

    HAnimHumanoid: utilize new collectChildNodes

--- src/nodes/H-Anim/HAnimHumanoid.js
+++ src/nodes/H-Anim/HAnimHumanoid.js
@@ -262,62 +262,8 @@ x3dom.registerNodeType(
             this.addField_MFVec3f( ctx, "skinBindingNormals", [] );
         },
         {
-            collectDrawableObjects : function ( transform, drawableCollection, singlePath, invalidateCache, planeMask, clipPlanes )
+            collectChildNodes : function ( childTransform, drawableCollection, singlePath, invalidateCache, planeMask, clipPlanes )
             {
-                // check if multi parent sub-graph, don't cache in that case
-                if ( singlePath && ( this._parentNodes.length > 1 ) )
-                {singlePath = false;}
-
-                // an invalid world matrix or volume needs to be invalidated down the hierarchy
-                if ( singlePath && ( invalidateCache = invalidateCache || this.cacheInvalid() ) )
-                {this.invalidateCache();}
-
-                this.collectBbox( transform, drawableCollection, singlePath, invalidateCache, planeMask, clipPlanes );
-
-                // check if sub-graph can be culled away or render flag was set to false
-                planeMask = drawableCollection.cull( transform, this.graphState(), singlePath, planeMask );
-                if ( planeMask < 0 )
-                {
-                    return;
-                }
-
-                var cnode,
-                    childTransform;
-
-                if ( singlePath )
-                {
-                    // rebuild cache on change and reuse world transform
-                    if ( !this._graph.globalMatrix )
-                    {
-                        this._graph.globalMatrix = this.transformMatrix( transform );
-                    }
-                    childTransform = this._graph.globalMatrix;
-                }
-                else
-                {
-                    childTransform = this.transformMatrix( transform );
-                }
-
-                var n = this._childNodes.length;
-
-                if ( x3dom.nodeTypes.ClipPlane.count > 0 )
-                {
-                    var localClipPlanes = [];
-
-                    for ( var j = 0; j < n; j++ )
-                    {
-                        if ( ( cnode = this._childNodes[ j ] ) )
-                        {
-                            if ( x3dom.isa( cnode, x3dom.nodeTypes.ClipPlane ) && cnode._vf.on && cnode._vf.enabled )
-                            {
-                                localClipPlanes.push( {plane: cnode, trafo: childTransform} );
-                            }
-                        }
-                    }
-
-                    clipPlanes = localClipPlanes.concat( clipPlanes );
-                }
-
                 //reset skin coords and normals before traversing skeleton
                 if ( this._cf.skinCoord.node )
                 {this._cf.skinCoord.node._vf.point.setValues( this._restCoords );}
