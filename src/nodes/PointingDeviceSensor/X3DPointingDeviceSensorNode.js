2fb8554fef628094734ef51882fd023cf0b090d5
Andreas Plesch <andreasplesch@users.noreply.github.com>
2026-04-22 16:42:48 -0400

    Refactor nameSpaceID assignment logic
    to avoid optional access

--- src/nodes/PointingDeviceSensor/X3DPointingDeviceSensorNode.js
+++ src/nodes/PointingDeviceSensor/X3DPointingDeviceSensorNode.js
@@ -87,11 +87,12 @@ x3dom.registerNodeType(
                 if ( this._vf.enabled )
                 {
                     this.postMessage( "isOver", true );
+                    const ns = this._nameSpace;
+                    const inline = ns.superInlineNode;
                     const nameSpaceID =
-                        this._nameSpace.superInlineNode?._DEF ||
-                        this._nameSpace.superInlineNode?._vf.url[0] ||
-                        this._nameSpace.name ||
-                        'name scope';
+                        inline && inline._DEF ? inline._DEF :
+                        inline ? inline._vf.url[0] :
+                        ns.name || 'name scope';
                     x3dom.debug.logInfo(
                         "Pointer sensed by " +
                         nameSpaceID + "/" +
